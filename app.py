83bb6fa29360ba1e2a7e1c5187735721e62e2d7a
https://github.com/Bhuvana6303 <bhuvanagundepalli>
2026-03-23 19:44:45 +0530

    fixed genai package

--- app.py
+++ app.py
@@ -96,8 +96,15 @@ def history():
     return render_template("history.html", history=chat_history)
 
 # ---------------- RUN ---------------- #
-import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "My app is working"
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
